d5442f88090ac46189498d6b43206c859da4fdff
alexdilo <aledankas@gmail.com>
2019-01-13 23:42:38 +0100

    final work

--- cpe.R
+++ cpe.R
@@ -5,24 +5,30 @@
 #              Arnau Sangra Rocamora - Data Driven Securty                     #
 #                                                                              #
 #******************************************************************************#
-if (!require("xml2")) {install.packages("xml2") ; library(xml2)}
+if (!require("xml2")) install.packages("xml2") ; library(xml2)
 
 compressed_cpes_url <- "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
 cpes_filename <- "cpes.zip"
-download.file(compressed_cpes_url, cpes_filename)
+download.file(compressed_cpes_url, cpes_filename)download.file(compressed_cpes_url, cpes_filename)
 unzip(zipfile = cpes_filename)
 cpe.file <- "./official-cpe-dictionary_v2.3.xml"
 
 GetCPEItems <- function(cpe.raw) {
-cpe.raw <- xml2::xml_find_all(cpes,".//d1:cpe-item")
-#extrac items to cpe.raw
+  cpes <- data.frame( name = xml2::xml_attr(xml2::xml_find_all(cpe.raw, "//d1:cpe-item"), "name"),
+                     title = xml2::xml_text(xml2::xml_find_all(cpe.raw, "//d1:cpe-item/@title")),
+                     cpe.23 = xml2::xml_text(xml2::xml_find_all(cpe.raw, "//cpe-23:cpe23-item/@name")),
+                     stringsAsFactors = F)
+  # return data frame
+
+  return(cpes)
+
 }
 
 CleanCPEs <- function(cpes){
-# extract and clean all the columns
-dataframe <- trimws(xml_text(cpes))
 
-  return(dataframe)
+  # data manipulation
+
+  return(cpes)
 }
 
 ParseCPEData <- function(cpe.file) {
